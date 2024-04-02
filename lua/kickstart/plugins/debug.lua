afd1267e275e19ae830dd7f7171bda295e68c15b
Yopaman <pabloinscriptions@gmail.com>
2024-04-02 19:06:01 +0200

    remove mason

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -15,10 +15,6 @@ return {
     'rcarriga/nvim-dap-ui',
     'nvim-neotest/nvim-nio',
 
-    -- Installs the debug adapters for you
-    'williamboman/mason.nvim',
-    'jay-babu/mason-nvim-dap.nvim',
-
     -- Add your own debuggers here
     'leoluz/nvim-dap-go',
   },
